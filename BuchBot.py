822f682a800060a35c169f0f071c778084f4a22b
Darren Struthers <dstruthers@LIBR17962.plu.edu>
2015-06-09 14:13:16 -0700

    Made configuration reloadable with !reload command

--- BuchBot.py
+++ BuchBot.py
@@ -1,27 +1,31 @@
 import ConfigParser, json, re
 from SlackBot import SlackBot
 
-config = ConfigParser.RawConfigParser()
-config.read('BuchBot.cfg')
-
-SLACK_API_TOKEN = config.get('General', 'api_token')
-SLACK_CHANNEL = config.get('General', 'slack_channel')
-KEYWORD_FILE = config.get('General', 'keyword_file')
-SEND_GREETINGS = config.getboolean('General', 'greet_people')
-
 slack_channel_id = None
 keyword_mappings = {}
 
+def load_config():
+    '''Load bot options from config file'''
+    global slack_api_token, slack_channel, keyword_file, send_greetings
+    config = ConfigParser.RawConfigParser()
+    config.read('BuchBot.cfg')
+
+    slack_api_token = config.get('General', 'api_token')
+    slack_channel = config.get('General', 'slack_channel')
+    keyword_file = config.get('General', 'keyword_file')
+    send_greetings = config.getboolean('General', 'greet_people')
+    
 def load_keywords():
+    '''Load keyword matching patterns from JSON file'''
     global keyword_mappings
-    f = open(KEYWORD_FILE, 'r')
+    f = open(keyword_file, 'r')
     keyword_mappings = json.loads(f.read())
     f.close()
 
 def on_open(bot):
     global slack_channel_id
     for channel in bot.channels:
-        if channel.name == SLACK_CHANNEL:
+        if channel.name == slack_channel:
             slack_channel_id = channel.id
             break
     bot.say(slack_channel_id, 'HELLO CLASS!!!')
@@ -40,10 +44,14 @@ def listen_for_keywords(bot, msg):
 def listen_for_commands(bot, msg):
     if msg.channel == slack_channel_id:
         if msg.text == '!reload':
+            load_config()
             load_keywords()
             bot.say(slack_channel_id, 'OKAY!!!')
 
 def greet_people(bot, msg):
+    if not send_greetings:
+        return
+    
     if msg.user == bot.user_id:
         return
     
@@ -57,13 +65,14 @@ def greet_people(bot, msg):
             bot.say(slack_channel_id, 'HELLO ' + user.username + '!!!')
     else:
         user.presence = msg.presence
-            
-buch = SlackBot(SLACK_API_TOKEN)
+
+load_config()
+
+buch = SlackBot(slack_api_token)
 buch.show_typing = True
 buch.add_event_listener('open', on_open)
 buch.add_event_listener('message', listen_for_keywords)
 buch.add_event_listener('message', listen_for_commands)
-if SEND_GREETINGS:
-    buch.add_event_listener('presence_change', greet_people)
+buch.add_event_listener('presence_change', greet_people)
 
 buch.run()
