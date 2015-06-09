3456b9fff4f0d039fbf1583a23c259096325b1fb
Darren Struthers <dstruthers@LIBR17962.plu.edu>
2015-06-09 11:41:52 -0700

    Moved a lot of boilerplate code into separate SlackBot module

--- BuchBot.py
+++ BuchBot.py
@@ -1,114 +1,44 @@
-import ConfigParser, json, re, urllib2, websocket
+import ConfigParser, re
+from SlackBot import SlackBot
 
 config = ConfigParser.RawConfigParser()
 config.read('BuchBot.cfg')
 
 SLACK_API_TOKEN = config.get('General', 'api_token')
-SLACK_API_BASE = config.get('General', 'api_base_uri')
 SLACK_CHANNEL = config.get('General', 'slack_channel')
+slack_channel_id = None
 
-def rtm_call(endpoint, **args):
-    '''Make Slack API call via RTM protocol'''
-    params = ''
-    for key in args:
-        if params: params += '&'
-        params += key + '=' + args[key]
-    url = SLACK_API_BASE + endpoint + '?' + params
-    result = json.loads(urllib2.urlopen(url).read())
-    return result
+buch = SlackBot(SLACK_API_TOKEN)
 
-channels = {}
-users = {}
-user_presence = {}
-channel_id = ''
-self_user_id = ''
-msg_id = 0
+def on_open(bot):
+    global slack_channel_id
+    for channel in bot.channels:
+        if channel.name == SLACK_CHANNEL:
+            slack_channel_id = channel.id
+            break
+    bot.say(slack_channel_id, 'HELLO CLASS!!!')
 
-start_result = rtm_call('rtm.start', token=SLACK_API_TOKEN)
-if not start_result['ok']:
-    print 'Could not initiate RTM session!'
-    exit(1)
-
-self_user_id = start_result['self']['id']
-
-for channel in start_result['channels']:
-    channels[channel['id']] = channel['name']
-    if channel['name'] == SLACK_CHANNEL:
-        channel_id = channel['id']
-
-for user in start_result['users']:
-    users[user['id']] = user['name']
+def on_message(bot, msg):
+    if msg.channel == slack_channel_id:
+        if re.search('bab(y|ies)', msg.text, re.I):
+            bot.say(slack_channel_id, 'DELICIOUS!!!')
+        elif re.search('(bathtub|monster truck tire)', msg.text, re.I):
+            bot.say(slack_channel_id, "I'VE BEEN WAITING FOR THIS ALL DAY!!!")
+        elif re.search('buchdawg', msg.text, re.I):
+            bot.say(slack_channel_id, 'WHO LET THE BUCH OUT, WHO, WHO WHO, WHO WHO')
+        elif re.search('cheeseburger', msg.text, re.I):
+            bot.say(slack_channel_id, "A MAN'S GOTTA EAT!!!")
+        elif re.search('child', msg.text, re.I):
+            bot.say(slack_channel_id, 'CHILD?? WHERE???')
+        elif re.search('electric', msg.text, re.I):
+            bot.say(slack_channel_id, "IF YOU'RE GOOD, I'LL TELL YOU A STORY ABOUT ELECTRICITY!!!")
+        elif re.search('fire drill', msg.text, re.I):
+            bot.say(slack_channel_id, "CAN SOMEBODY CARRY ME?!?!")
+        elif re.search('food', msg.text, re.I):
+            bot.say(slack_channel_id, 'SOMEONE SAY FOOD?? I COULD EAT...')
+        elif re.search('kinderwurst', msg.text, re.I):
+            bot.say(slack_channel_id, "I'M GETTING HUNGRY!!!")
     
-# WebSocket callbacks
-def on_message(ws, message):
-    print message
-    msg = json.loads(message)
-    if msg['type'] == 'presence_change':
-        if msg['presence'] == 'active':
-            if not user_presence.has_key(msg['user']) or user_presence[msg['user']] != 'active':
-                user_presence[msg['user']] = 'active'
-                user_name = users[msg['user']]
-                if user_name != 'mr_buchanan':
-                    say(ws, 'HELLO ' + user_name + '!')
-        elif msg['presence'] == 'away':
-            user_presence[msg['user']] = 'away'
-    elif msg['type'] == 'message':
-        if msg['channel'] == channel_id:
-            if msg['text'][0] == '!':
-                match = re.match('^!([a-z]+)(.*)$', msg['text'])
-                command = match.groups()[0]
-                if command == 'buchanan':
-                    emoji = ':buchflower: '
-                    m = ''
-                    for i in [1,2,3,2,1]:
-                        for j in range(0, i):
-                            m += emoji
-                        m += '\n'
-                    say(ws, m)
-            else:
-                if re.search('bab(y|ies)', msg['text'], re.I):
-                    say(ws, 'DELICIOUS!!!')
-                elif re.search('(bathtub|monster truck tire)', msg['text'], re.I):
-                    say(ws, "I'VE BEEN WAITING FOR THIS ALL DAY!!!")
-                elif re.search('buchdawg', msg['text'], re.I):
-                    say(ws, 'WHO LET THE BUCH OUT, WHO, WHO WHO, WHO WHO')
-                elif re.search('cheeseburger', msg['text'], re.I):
-                    say(ws, "A MAN'S GOTTA EAT!!!")
-                elif re.search('child', msg['text'], re.I):
-                    say(ws, 'CHILD?? WHERE???')
-                elif re.search('electricity', msg['text'], re.I):
-                    say(ws, "IF YOU'RE GOOD, I'LL TELL YOU A STORY ABOUT ELECTRICITY!!!")
-                elif re.search('fire drill', msg['text'], re.I):
-                    say(ws, "CAN SOMEBODY CARRY ME?!?!")
-                elif re.search('food', msg['text'], re.I):
-                    say(ws, 'SOMEONE SAY FOOD?? I COULD EAT...')
-                elif re.search('kinderwurst', msg['text'], re.I):
-                    say(ws, "I'M GETTING HUNGRY!!!")
-
-def on_error(ws, error):
-    print 'Error: ' + error
-
-def on_close(ws):
-    say(ws, 'GOOD-BYE!!!')
-
-def on_open(ws):
-    say(ws, 'HELLO CLASS!')
-
-def next_id():
-    global msg_id
-    msg_id += 1
-    return msg_id
-    
-def say(ws, text):
-    msg = json.dumps({'id': next_id(), 'type': 'message', 'channel': channel_id, 'text': text})
-    ws.send(msg)
-    
-websocket.enableTrace(True)
-ws = websocket.WebSocketApp(start_result['url'],
-                            on_message = on_message,
-                            on_error = on_error,
-                            on_close = on_close,
-                            on_open = on_open)
-
-ws.run_forever()
-
+buch.add_event_listener('open', on_open)
+buch.add_event_listener('message', on_message)
+buch.run()
---
+++ SlackBot.py
@@ -0,0 +1,150 @@
+import json, urllib2, websocket
+
+class SlackBot:
+    '''Uses Slack RTM and WebSocket APIs to provide basic slack bot
+    functionality'''
+
+    SLACK_API_BASE = 'https://slack.com/api/'
+    
+    def __init__(self, api_token):
+        '''SlackBot constructor'''
+        self.api_token = api_token
+        self.event_listeners = {}
+        self.message_id = 0
+
+    def add_event_listener(self, type, fn):
+        '''Add high level event listener'''
+        if not self.event_listeners.has_key(type):
+            self.event_listeners[type] = []
+        self.event_listeners[type].append(fn)
+
+    def say(self, channel_id, text):
+        msg = json.dumps({'id': self.next_message_id(),
+                          'type': 'message',
+                          'channel': channel_id,
+                          'text': text})
+        self.ws.send(msg)
+
+    def next_message_id(self):
+        '''Returns unique id for use in sending messages'''
+        self.message_id += 1
+        return self.message_id
+
+    @staticmethod
+    def rtm_call(endpoint, **args):
+        '''Make Slack API call via RTM protocol'''
+        params = ''
+        for key in args:
+            if params: params += '&'
+            params += key + '=' + args[key]
+        url = SlackBot.SLACK_API_BASE + endpoint + '?' + params
+        result = json.loads(urllib2.urlopen(url).read())
+        return result
+
+    # Child classes of SlackBot may wish to override these methods
+    def on_open(self, ws): pass
+    def on_message(self, ws, message): pass
+    def on_error(self, ws, error): pass
+    def on_close(self, ws): pass
+
+    def run(self):
+        def on_open(ws):
+            self.on_open(ws)
+            if self.event_listeners.has_key('open'):
+                for fn in self.event_listeners['open']:
+                    fn(self)
+
+        def on_message(ws, message):
+            self.on_message(ws, message)
+            e = SlackEvent.from_dict(json.loads(message))
+            if self.event_listeners.has_key(e.type):
+                for fn in self.event_listeners[e.type]:
+                    fn(self, e)
+                    
+        def on_error(ws, error):
+            self.on_error(ws, error)
+        def on_close(ws):
+            self.on_close(ws)
+
+        start_result = SlackBot.rtm_call('rtm.start', token=self.api_token)
+        if start_result['ok']:
+            self.user_id = start_result['self']['id']
+            self.user_username = start_result['self']['name']
+            self.user_prefs = start_result['self']['prefs']
+            self.users = []
+            for user in start_result['users']:
+                self.users.append(SlackUser.from_dict(user))
+
+            self.channels = []
+            for channel in start_result['channels']:
+                self.channels.append(SlackChannel.from_dict(channel))   
+
+            websocket.enableTrace(True)
+            self.ws = websocket.WebSocketApp(start_result['url'],
+                                             on_message = on_message,
+                                             on_error = on_error,
+                                             on_close = on_close,
+                                             on_open = on_open)
+            self.ws.run_forever()
+        else:
+            raise SlackBotError('Could not initiate RTM session')
+
+class SlackUser:
+    '''Basic representation of a Slack user'''
+    @staticmethod
+    def from_dict(d):
+        '''Creates a SlackUser object from a dictionary of attributes.
+        Suitable for use with objects returned by Slack RTM API.'''
+
+        u = SlackUser()
+        u.id = d['id']
+        u.username = d['name']
+        u.presence = d['presence']
+        if d.has_key('profile'):
+            if d['profile'].has_key('email'):
+                u.email = d['profile']['email']
+            if d['profile'].has_key('first_name'):
+                u.first_name = d['profile']['first_name']
+            if d['profile'].has_key('last_name'):
+                u.last_name = d['profile']['last_name']
+            if d['profile'].has_key('real_name'):
+                u.real_name = d['profile']['real_name']
+        return u
+
+class SlackChannel:
+    '''Basic representation of a Slack chat channel'''
+    @staticmethod
+    def from_dict(d):
+        '''Creates a SlackChannel object from a dictionary of attributes.
+        Suitable for use with objects return by Slack RTM API.'''
+
+        c = SlackChannel()
+        c.id = d['id']
+        c.is_archived = d['is_archived']
+        c.is_general = d['is_general']
+        if d.has_key('members'):
+            c.members = d['members']
+        c.name = d['name']
+        if d.has_key('topic') and d['topic'].has_key('value'):
+            c.topic = d['topic']['value']
+        return c
+    
+class SlackEvent:
+    '''Basic representation of a Slack event object'''
+
+    @staticmethod
+    def from_dict(d):
+        e = SlackEvent()
+        e.type = d['type']
+
+        if d['type'] == 'message':
+            e.ts = d['ts']
+            e.user = d['user']
+            e.channel = d['channel']
+            e.text = d['text']
+        elif d['type'] == 'presence_change':
+            e.user = d['user']
+            e.presence = d['presence']
+        return e
+
+class SlackBotError(Exception): pass
