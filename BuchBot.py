0fc4c16264f06aa176c03a7231ab7599cd414acf
Darren Struthers <dstruthers@LIBR17962.plu.edu>
2015-06-09 12:01:16 -0700

    Moved keyword matches into a JSON file

--- BuchBot.py
+++ BuchBot.py
@@ -1,4 +1,4 @@
-import ConfigParser, re
+import ConfigParser, json, re
 from SlackBot import SlackBot
 
 config = ConfigParser.RawConfigParser()
@@ -6,9 +6,18 @@ config.read('BuchBot.cfg')
 
 SLACK_API_TOKEN = config.get('General', 'api_token')
 SLACK_CHANNEL = config.get('General', 'slack_channel')
+KEYWORD_FILE = config.get('General', 'keyword_file')
+
 slack_channel_id = None
+keyword_mappings = {}
 
 buch = SlackBot(SLACK_API_TOKEN)
+
+def load_keywords():
+    global keyword_mappings
+    f = open(KEYWORD_FILE, 'r')
+    keyword_mappings = json.loads(f.read())
+    f.close()
 
 def on_open(bot):
     global slack_channel_id
@@ -19,25 +28,14 @@ def on_open(bot):
     bot.say(slack_channel_id, 'HELLO CLASS!!!')
 
 def on_message(bot, msg):
+    global keyword_mappings
     if msg.channel == slack_channel_id:
-        if re.search('bab(y|ies)', msg.text, re.I):
-            bot.say(slack_channel_id, 'DELICIOUS!!!')
-        elif re.search('(bathtub|monster truck tire)', msg.text, re.I):
-            bot.say(slack_channel_id, "I'VE BEEN WAITING FOR THIS ALL DAY!!!")
-        elif re.search('buchdawg', msg.text, re.I):
-            bot.say(slack_channel_id, 'WHO LET THE BUCH OUT, WHO, WHO WHO, WHO WHO')
-        elif re.search('cheeseburger', msg.text, re.I):
-            bot.say(slack_channel_id, "A MAN'S GOTTA EAT!!!")
-        elif re.search('child', msg.text, re.I):
-            bot.say(slack_channel_id, 'CHILD?? WHERE???')
-        elif re.search('electric', msg.text, re.I):
-            bot.say(slack_channel_id, "IF YOU'RE GOOD, I'LL TELL YOU A STORY ABOUT ELECTRICITY!!!")
-        elif re.search('fire drill', msg.text, re.I):
-            bot.say(slack_channel_id, "CAN SOMEBODY CARRY ME?!?!")
-        elif re.search('food', msg.text, re.I):
-            bot.say(slack_channel_id, 'SOMEONE SAY FOOD?? I COULD EAT...')
-        elif re.search('kinderwurst', msg.text, re.I):
-            bot.say(slack_channel_id, "I'M GETTING HUNGRY!!!")
+        if keyword_mappings == {}:
+            load_keywords()
+            
+        for pattern in keyword_mappings:
+            if re.search(pattern, msg.text, re.I):
+                bot.say(slack_channel_id, keyword_mappings[pattern])
     
 buch.add_event_listener('open', on_open)
 buch.add_event_listener('message', on_message)
